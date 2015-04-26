40c9e97c377acdec4d0a468bfffcaa0c9a64b4c2
shimba <is0020he@ed.ritsumei.ac.jp>
2015-04-26 16:16:30 +0900

    rewrite perceptron.py python like code

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -23,7 +23,7 @@ ones = np.ones((n, 1))
 X = np.hstack((ones, data))
 
 # put labels into variable y
-y = targets
+y = targets.reshape(n)
 y[y == 0] = -1  # zero to -1
 
 X_train, X_valid, y_train, y_valid = cross_validation.train_test_split(X, y)
@@ -31,27 +31,20 @@ X_train, X_valid, y_train, y_valid = cross_validation.train_test_split(X, y)
 n_train = len(X_train)
 n_valid = len(X_valid)
 
-w = np.zeros(65)
+w = np.zeros_like(X[0])
 
-
-def f(a):
-    if a >= 0:
-        return 1
-    else:
-        return -1
-
-for itr in range(5):
-    print "iteration", itr
+for r in range(5):
+    print "iteration", r
     for xi, yi in zip(X_train, y_train):
-        if f(np.dot(xi, w)) != yi:
+        if np.sign(np.dot(xi, w)) != yi:
             w += eta * xi * yi
             # print "w", w[0]
 
-    count_fails = 0
-    for xi, yi in zip(X_valid, y_valid):  # validation data
-        if f(np.dot(xi, w)) != yi:
-            count_fails += 1
-    print "error rate", count_fails / float(n_valid)
+    # validation
+    xw = np.dot(X_valid, w)
+    predicted = np.sign(xw)
 
-plt.matshow(w[1:].reshape(8, 8), cmap=plt.cm.gray)
-print w[1:].reshape(8, 8)
+    print "error rate", np.sum(predicted != y_valid) / float(n_valid)
+
+# plt.matshow(w[1:].reshape(8, 8), cmap=plt.cm.gray)
+# print w[1:].reshape(8, 8)
