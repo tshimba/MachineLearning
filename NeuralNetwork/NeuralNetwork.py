3a70cb3a28b0919421239868babab3c582a782cc
f2um2326 <f2um2326@gmail.com>
2015-05-25 09:26:16 +0900

    Parameter tuning: set momentum coefficient to 0.1 and regularization coefficient to 0.1. // iteration 600 / [train] 0.9991 / [valid] 0.9798 / Best model: r = 593, correct rate = 0.980500 / [test] correct rate  0.9813

--- NeuralNetwork/NeuralNetwork.py
+++ NeuralNetwork/NeuralNetwork.py
@@ -268,7 +268,7 @@ if __name__ == "__main__":
     classifier = NeuralNetworkClassifier(M=600)
     classifier.fit(data_train, label_train, data_valid, label_valid,
                    lr=0.0003, num_iteration=600, minibatch_size=500,
-                   mc=0.0, regularization=0.0, std_w1_init=0.0001,
+                   mc=0.1, regularization=0.1, std_w1_init=0.0001,
                    std_w2_init=0.0001)
 
     # -- test -- #
