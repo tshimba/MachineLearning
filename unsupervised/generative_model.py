72b9cc666a7dd864f47982247416a2b08c09095e
f2um2326 <f2um2326@gmail.com>
2015-07-15 17:51:10 +0900

    Assume the average of sampled data from Poisson distribution and resample it by using the average as the parameter of Poisson distribution.

--- unsupervised/generative_model.py
+++ unsupervised/generative_model.py
@@ -5,7 +5,20 @@ Created on Wed Jul 15 00:03:47 2015
 @author: shimba
 """
 
+import numpy as np
 from utils import basic_distributions
+import matplotlib.pyplot as plt
+
+
+def my_poisson(lam):
+    n_s = 100000
+    s = np.random.poisson(lam, n_s)
+    plt.plot(range(n_s), s, '.')
+    plt.show()
+
+    plt.hist(s, 14, normed=True)
+    plt.xlim([-2, 10])
+    plt.show()
 
 if __name__ == '__main__':
     distributions = [basic_distributions.Gaussian(),  # 0
@@ -20,3 +33,6 @@ if __name__ == '__main__':
 
     sampler.visualize(x)
     print "Distribution: ", sampler.get_name()
+
+    average = x.sum().astype(np.float32) / len(x)
+    my_poisson(average.round())
