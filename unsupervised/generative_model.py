fab29aa54dd928b95d5f7e7a9e129af5ac267cb5
f2um2326 <f2um2326@gmail.com>
2015-08-03 15:23:31 +0900

    Implementation for prediction of distribution #4 LinearModel.

--- unsupervised/generative_model.py
+++ unsupervised/generative_model.py
@@ -29,13 +29,37 @@ if __name__ == '__main__':
                      basic_distributions.LinearModel2(),    # 5
                      basic_distributions.PoissonGLM(),      # 6
                      ]
-    dist_type = 1
+    dist_type = 4
     sampler = distributions[dist_type]
 
-    x = sampler()
+    m = c = error = 0
+    n_iteration = 2000
+    for i in range(n_iteration):
+        y = sampler()
+        print 'Iteration:', i + 1, '/', n_iteration
 
-    sampler.visualize(x)
+        # Calculate slope, intercept and error
+        N = len(y)
+        x = np.arange(0, N)
+        A = np.vstack([x, np.ones(N)]).T
+
+        lsm = np.linalg.lstsq(A, y)
+        m = m + lsm[0][0]
+        c = c + lsm[0][1]
+        error = error + np.sqrt(lsm[1][0] / N)
+
+    m = m / n_iteration
+    c = c / n_iteration
+    error = error / n_iteration
+
+    # Plot
+    # Original
+    sampler.visualize(y)
     print "Distribution: ", sampler.get_name()
 
-    average = x.sum().astype(np.float32) / len(x)
-    my_poisson(average)
+    # Predicted
+    x_plot = np.arange(0, N)
+    y_plot = (m * x_plot + c)
+    noise = np.random.randn(N) * error
+    y_plot = y_plot + noise
+    sampler.visualize(y_plot)
