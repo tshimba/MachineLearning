86dd5b80bf0ef9d133da17b6e10c84493ba0dfdb
shimba <is0020he@ed.ritsumei.ac.jp>
2015-08-14 15:52:03 +0900

    Implementation to call mixture distribution sampler.

--- unsupervised/generative_model.py
+++ unsupervised/generative_model.py
@@ -6,7 +6,7 @@ Created on Wed Jul 15 00:03:47 2015
 """
 
 import numpy as np
-from utils import basic_distributions
+from utils import mixture_distributions as mix
 import matplotlib.pyplot as plt
 
 
@@ -21,45 +21,14 @@ def my_poisson(lam):
     plt.show()
 
 if __name__ == '__main__':
-    distributions = [basic_distributions.Gaussian(),        # 0
-                     basic_distributions.Poisson(),         # 1
-                     basic_distributions.Gamma(),           # 2
-                     basic_distributions.Beta(),            # 3
-                     basic_distributions.LinearModel(),     # 4
-                     basic_distributions.LinearModel2(),    # 5
-                     basic_distributions.PoissonGLM(),      # 6
+    distributions = [mix.MixtureOfGaussians(),  # 0
+                     mix.MixtureOfPoissons(),   # 1
                      ]
-    dist_type = 4
+    dist_type = 0
     sampler = distributions[dist_type]
 
-    m = c = error = 0
-    n_iteration = 2000
-    for i in range(n_iteration):
-        y = sampler()
-        print 'Iteration:', i + 1, '/', n_iteration
+    x = sampler(10000)
 
-        # Calculate slope, intercept and error
-        N = len(y)
-        x = np.arange(0, N)
-        A = np.vstack([x, np.ones(N)]).T
-
-        lsm = np.linalg.lstsq(A, y)
-        m = m + lsm[0][0]
-        c = c + lsm[0][1]
-        error = error + np.sqrt(lsm[1][0] / N)
-
-    m = m / n_iteration
-    c = c / n_iteration
-    error = error / n_iteration
-
-    # Plot
-    # Original
-    sampler.visualize(y)
+    sampler.visualize(x)
     print "Distribution: ", sampler.get_name()
-
-    # Predicted
-    x_plot = np.arange(0, N)
-    y_plot = (m * x_plot + c)
-    noise = np.random.randn(N) * error
-    y_plot = y_plot + noise
-    sampler.visualize(y_plot)
+    print "Parameters: ", sampler.get_params()
