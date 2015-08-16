4cd0fa92c53be44719e22328b9c3a0629af70365
shimba <is0020he@ed.ritsumei.ac.jp>
2015-08-16 23:20:19 +0900

    Show predicted result.

--- unsupervised/complete_mixture_model.py
+++ unsupervised/complete_mixture_model.py
@@ -7,8 +7,6 @@ Created on Wed Jul 15 00:03:47 2015
 
 import numpy as np
 from utils import mixture_distributions as mix
-import matplotlib.pyplot as plt
-
 
 if __name__ == '__main__':
     distributions = [mix.MixtureOfGaussians(),  # 0
@@ -19,6 +17,7 @@ if __name__ == '__main__':
 
     x = sampler(10000, complete_data=True)
 
+    # Show sampled original data
     sampler.visualize(x[1])
     print "Distribution: ", sampler.get_name()
     print "Parameters: ", sampler.get_params()
@@ -43,3 +42,10 @@ if __name__ == '__main__':
         means[i] = np.sum(each_class_x[i]) / sum_each_class[i]
         stds[i] = np.sqrt(np.sum((each_class_x[i] - means[i]) ** 2) /
                           sum_each_class[i])
+
+    predicted_sampler = mix.MixtureOfGaussians(K=N_classes, weights=weights,
+                                               means=means, stds=stds)
+    px = predicted_sampler(num_examples=N)
+
+    # Show sampled data by using predicted parameters
+    sampler.visualize(px)
